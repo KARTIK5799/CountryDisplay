cee63da0276199d7fd040c6ca58a8d13e04292b8
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-22 16:47:57 +0530

    cd

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,50 +1,64 @@
-import React from 'react'
-import { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
 const Country = () => {
-    const [apiData, setApiData] = useState(null);
+  const [apiData, setApiData] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch('https://restcountries.com/v3.1/all');
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-  
-          const data = await response.json();
-          setApiData(data);
-        } catch (error) {
-          console.error('Error fetching data:', error);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
-      };
-  
-      fetchData();
-    }, []);
-  
-    return (
-      <div className="app p-6 bg-gray-100 flex flex-col items-center">
-        <h1 className="text-3xl font-bold mb-4">Country Information</h1>
-        {apiData ? (
-          <div className="flex flex-wrap justify-center">
-            {apiData.map((country) => (
-              <div
-                key={country.cca3}
-                className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg mx-4 my-4 flex flex-col items-center w-72 h-72"
-              >
-                <img
-                  src={country.flags.png}
-                  alt={`Flag of ${country.name.common}`}
-                  className="w-full h-32 object-cover mb-4 rounded-md"
-                />
-                <h3 className="text-xl font-semibold mb-2">{country.name.common}</h3>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-    );
-}
 
-export default Country
+        const data = await response.json();
+        setApiData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const filteredCountries = apiData
+    ? apiData.filter((country) =>
+        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : [];
+
+  return (
+    <div className="app p-6 bg-gray-100 flex flex-col items-center">
+      <h1 className="text-3xl font-bold mb-4">Country Information</h1>
+      <input
+        type="text"
+        placeholder="Search for a country..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="p-2 mb-4 border rounded-md"
+      />
+      {filteredCountries.length > 0 ? (
+        <div className="flex flex-wrap justify-center">
+          {filteredCountries.map((country) => (
+            <div
+              key={country.cca3}
+              className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg mx-2 my-4 flex flex-col items-center w-32 sm:w-40 md:w-48 lg:w-56 xl:w-64"
+            >
+              <img
+                src={country.flags.png}
+                alt={`Flag of ${country.name.common}`}
+                className="w-full h-32 object-cover mb-4 rounded-md"
+              />
+              <h3 className="text-xl font-semibold mb-2">{country.name.common}</h3>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No matching countries found.</p>
+      )}
+    </div>
+  );
+};
+
+export default Country;
