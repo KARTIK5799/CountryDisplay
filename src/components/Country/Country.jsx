a943e52c1776184ce30624dc3bbc29670e042e6f
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-23 01:10:32 +0530

    cjs

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,23 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import styles from './Country.module.css'
+import styles from './Country.module.css';
 
 const Country = () => {
   const [apiData, setApiData] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
- 
-
 
   const fetchData = async () => {
     try {
       const response = await fetch('https://restcountries.com/v3.1/all');
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
       const data = await response.json();
       setApiData(data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error(error);
     }
   };
 
@@ -25,47 +19,37 @@ const Country = () => {
     fetchData();
   }, []);
 
-
   const filteredCountries = apiData
     ? apiData.filter((country) =>
         country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
       )
     : [];
 
- 
-    return (
-      <div className={styles.searchContainer}>
-        
-          <input
-            type="text"
-            placeholder="Search for a countries..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className={styles.searchInput}
-          />
-   
-        <div>
-          {filteredCountries.length > 0 ? (
-            <div className={styles.flagSection}>
-              {filteredCountries.map((country) => (
-                <div key={country.flags.png}className={styles.countryContainer}>
-                 
-                  <img
-                    src={country.flags.png}
-                    alt={`flag`}
-                    className={styles.countryImage}
-                  />
-                  <div className={styles.countryName}><h4>{country.name.common}</h4></div>
-                </div>
-              ))}
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        type="text"
+        placeholder="Search for a country..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className={styles.searchInput}
+      />
+      <div className={styles.flagSection}>
+        {filteredCountries.map((country, index) => (
+          <div key={index} className={styles.countryContainer}>
+            <img
+              src={country.flags?.png || ''}
+              alt={`flag`}
+              className={styles.countryImage}
+            />
+            <div className={styles.countryName}>
+              <h2>{country.name.common}</h2>
             </div>
-          ) : (
-            ""
-          )}
-        </div>
+          </div>
+        ))}
       </div>
-    );
-  
+    </div>
+  );
 };
 
 export default Country;
